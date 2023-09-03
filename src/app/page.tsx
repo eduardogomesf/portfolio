8b63c6fcfaa583f8f6ef89d7a8fd56fa5b0011cf
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-03 17:08:42 -0300

    fix: remove horizontal margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ExternalLinkButton } from "./_components/ExternalLinkButton";
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-[#020c1b] w-screen	h-screen items-center	justify-center mx-3">
+    <main className="flex flex-col bg-[#020c1b] w-screen	h-screen items-center	justify-center">
 
       <Image src="/images/me.jpg" alt="Avatar" width={300} height={300} className="rounded-full border-2 border-[#C0C0C0]" />
 
