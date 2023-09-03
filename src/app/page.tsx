c545866c44fa40b534d399da67eb1b568cb65b07
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-03 17:22:48 -0300

    feat: decrease gap size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
         className="rounded-full border-2 border-[#C0C0C0]"
       />
 
-      <div className="mt-6 flex flex-col items-center justify-center gap-4">
+      <div className="mt-6 flex flex-col items-center justify-center gap-3">
         <strong className="text-[#FFFFFF] text-2xl sm:text-4xl">Eduardo Gomes</strong>
         <span className="text-[#B0B0B0] text-xl sm:text-2xl" >Full Stack Software Developer</span>
       </div>
 
-      <div className="mt-6 flex flex-col items-center justify-center gap-4">
+      <div className="mt-6 flex flex-col items-center justify-center gap-3">
         <ExternalLinkButton text="Linkedin" link="https://www.linkedin.com/in/eduardogomesf/" />
         <ExternalLinkButton text="Github" link="https://github.com/eduardogomesf" />
         <ExternalLinkButton text="Stackoverflow" link="https://stackoverflow.com/users/11420127/eduardo" />
