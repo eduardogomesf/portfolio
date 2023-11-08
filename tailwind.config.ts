fcebc2bc80cd39d7aa8d77932558ff5e17fb51d8
Eduardo Gomes <edugomesf@icloud.com>
2023-11-07 22:45:47 -0300

    feat: add color palette for light and dark themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        '01': '#181818',
+        '02': '#1B1B1B',
+        '03': '#282828',
+        '04': '#303030',
+        '05': '#373737',
+        '06': '#3F3F3F',
+        '07': '#4A4A4A',
+        '08': '#606060',
+        '09': '#6E6E6E',
+        '10': '#818181',
+        '11': '#B1B1B1',
+        '12': '#EEEEEE',
+        '01-light': '#FCFCFC',
+        '02-light': '#F9F9F9',
+        '03-light': '#F1F1F1',
+        '04-light': '#EBEBEB',
+        '05-light': '#E4E4E4',
+        '06-light': '#DDDDDD',
+        '07-light': '#D4D4D4',
+        '08-light': '#BBBBBB',
+        '09-light': '#8D8D8D',
+        '10-light': '#808080',
+        '11-light': '#646464',
+        '12-light': '#202020'
+      }
     },
   },
   plugins: [],
